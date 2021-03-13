a799a23251f41d190061d24a21fb6bbc3c156361
dasrys <dasrys@wp.pl>
2021-03-13 18:15:45 +0100

    first commit

--- blog/models.py
+++ blog/models.py
@@ -12,7 +12,9 @@ class Post(models.Model):
         default=timezone.now)
     published_date = models.DateTimeField(
         blank=True, null=True)
-
+    img = models.ImageField(blank=True, null=True,
+        upload_to='static/media/image')
+    
     def publish(self):
         self.published_date = timezone.now()
         self.save()
